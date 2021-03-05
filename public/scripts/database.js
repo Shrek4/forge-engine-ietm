220e28c6062683f1e80e4374de9037d48568900e
Михаил Строганов 171-334 <46332122+Shrek4@users.noreply.github.com>
2021-03-05 17:48:59 +0400

    sas

--- public/scripts/database.js
+++ public/scripts/database.js
@@ -1,21 +1,11 @@
 async function showPartDescription(id) {
     $.get( "http://localhost:3000/parts", function( data ) {
       $( "#partdesc" ).html( data[id].description );
-<<<<<<< HEAD
-      //console.log( data );
-=======
->>>>>>> b24a0be4f5250e64d7c4f34bac0fb4f2ee34161a
     });
 }
 
 async function showEngineDescription() {
-<<<<<<< HEAD
     $.get( "http://localhost:3000/parts", function( data ) {
       $( "#info" ).html( data[0].description );
     });
-=======
-  $.get( "http://localhost:3000/parts", function( data ) {
-    $( "#info" ).html( data[0].description );
-  });
->>>>>>> b24a0be4f5250e64d7c4f34bac0fb4f2ee34161a
 }
--- public/scripts/viewer.js
+++ public/scripts/viewer.js
@@ -113,15 +113,6 @@ function onDocumentLoadFailure() {
     console.error("Failed fetching Forge manifest");
 }
 
-<<<<<<< HEAD
-function viewerclick(){
-    if(document.getElementById('partdesc')) {
-        let shrek=viewer.getSelection()[0];
-        if(shrek) showPartDescription(shrek);
-    }
-    let shrek=viewer.getSelection()[0];
-    console.log(shrek)
-=======
 function onViewerClick(){
     if(document.getElementById('partdesc')) {
         let shrek = viewer.getSelection()[0];
@@ -130,5 +121,4 @@ function onViewerClick(){
             showPartDescription(shrek);
         }
     }
->>>>>>> b24a0be4f5250e64d7c4f34bac0fb4f2ee34161a
 }
