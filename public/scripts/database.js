74d92ea5ca10d3fc0388a0d5b20adf95641be05e
shrekislove <skarrbonov@gmail.com>
2021-06-21 00:20:27 +0400

    sas

--- public/scripts/database.js
+++ public/scripts/database.js
@@ -1,4 +1,4 @@
-const socket = "http://localhost:3000";
+const socket = "https://forge-ietm.herokuapp.com/";
 
 async function showPartDescription(id) { //показывает описание детали
     $.get( socket + "/components", function (data) {
